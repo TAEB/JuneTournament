7b1a8093852ec0a04b0e323ad2b99ba8c8c64264
sartak <sartak@gmail.com>
2007-02-03 18:51:59 -0500

    Add fullupdate.sh which copies everything over to katron.org

---
+++ fullupdate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+perl trophies.pl
+tar -jcf pages.tbz2 player.css player/* clan/*
+scp pages.tbz2 katron.org:public_html/nh/07/
+rm pages.tbz2
+ssh katron.org 'cd public_html/nh/07 && tar -jxf pages.tbz2 && rm pages.tbz2'
+
