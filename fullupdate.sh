b2c5d44e9c9069f2bc579007ff134090bbc4135c
sartak <sartak@gmail.com>
2007-02-03 19:29:22 -0500

    Add more output to fullupdate.sh

--- fullupdate.sh
+++ fullupdate.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-perl trophies.pl
+time perl trophies.pl
+echo "Compressing all pages"
 tar -jcf pages.tbz2 player.css player/* clan/* trophy/*
+echo "Uploading tarball"
 scp pages.tbz2 katron.org:public_html/nh/07/
 rm pages.tbz2
+echo "Extracting tarball remotely"
 ssh katron.org 'cd public_html/nh/07 && tar -jxf pages.tbz2 && rm pages.tbz2'
 
