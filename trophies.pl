da4e569f72cfab726d2e516b11b3caed2d8c8c6d
sartak <sartak@gmail.com>
2007-03-30 16:41:23 -0400

    Add an xlogfile.unsure for ascensions without dumplogs

--- trophies.pl
+++ trophies.pl
@@ -577,7 +577,7 @@ sub main # {{{
   print "Reading clan_info\n";
   read_clan_info("clan_info");
   print "Reading xlogfile\n";
-  read_xlogfile("xlogfile");
+  read_xlogfile("xlogfile"", "xlogfile.unsure");
 
 # read_xlogfile populates %txt_output_for's keys with each player
 # so we put any initial text for each player's page here
