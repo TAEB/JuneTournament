72193c6cca35a97bad2f93e26799a649bc3f92db
sartak <sartak@gmail.com>
2007-03-30 16:21:02 -0400

    Add a script to download new logfile entries into a file

---
+++ tail_logfile.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $input = 'http://alt.org/nethack/logfile';
+my $output = 'intermediate_logfile';
+
+exec("tail -n0 -s0.3 -f logfile > $output") unless fork;
+
+system("wget -qc $input");
+sleep 1; # make sure tail sees the new output before we kill it
+kill -9 => $$;
+
