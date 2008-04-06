437b6401682425a86c042a4e3043ba15d4a5f519
Shawn M Moore <sartak@gmail.com>
2008-04-06 05:34:01 -0400

    Include recent ascensions and recent games on /

--- lib/JuneTournament/View.pm
+++ lib/JuneTournament/View.pm
@@ -14,6 +14,19 @@ template '/' => page {
         );
         outs ".";
     }
+
+    h3 { "Recent Ascensions" }
+    render_region(
+        path => 'recent-ascensions',
+        name => 'recent-ascensions',
+    );
+
+    h3 { "Recent Games" }
+    render_region(
+        path => 'recent-games',
+        name => 'recent-games',
+    );
+
 };
 
 template 'recent-ascensions' => sub {
