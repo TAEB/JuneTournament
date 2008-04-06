5e8c9cefb8c7fec725b133619a18bd0df0da880d
Shawn M Moore <sartak@gmail.com>
2008-04-06 00:33:32 -0400

    Add mandatory constraints on the columns of Game in the vanilla logfile

--- lib/JuneTournament/Model/Game.pm
+++ lib/JuneTournament/Model/Game.pm
@@ -6,46 +6,65 @@ use Jifty::DBI::Schema;
 
 use JuneTournament::Record schema {
     column player =>
-        refers_to JuneTournament::Model::Player by 'name';
+        refers_to JuneTournament::Model::Player by 'name',
+        is mandatory;
     column version =>
-        type is 'text';
+        type is 'text',
+        is mandatory;
     column score =>
-        type is 'int';
+        type is 'int',
+        is mandatory;
     column branch =>
         type is 'text',
-        valid_values are qw(dungeons mines sokoban quest ludios gehennom vlad planes);
+        valid_values are qw(dungeons mines sokoban quest ludios gehennom vlad planes),
+        is mandatory;
     column curlvl =>
-        type is 'int';
+        type is 'int',
+        is mandatory;
     column maxlvl =>
-        type is 'int';
+        type is 'int',
+        is mandatory;
     column curhp =>
-        type is 'int';
+        type is 'int',
+        is mandatory;
     column maxhp =>
-        type is 'int';
+        type is 'int',
+        is mandatory;
     column deaths =>
-        type is 'int';
+        type is 'int',
+        is mandatory;
     column enddate =>
-        type is 'text';
+        type is 'text',
+        is mandatory;
     column startdate =>
-        type is 'text';
+        type is 'text',
+        is mandatory;
     column uid =>
-        type is 'int';
+        type is 'int',
+        is mandatory;
     column role =>
         type is 'text',
-        valid_values are qw(Arc Bar Cav Hea Kni Mon Pri Ran Rog Sam Tou Val Wiz);
+        valid_values are qw(Arc Bar Cav Hea Kni Mon Pri Ran Rog Sam Tou Val Wiz),
+        is mandatory;
     column race =>
         type is 'text',
-        valid_values are qw(Hum Elf Orc Gno Dwa);
+        valid_values are qw(Hum Elf Orc Gno Dwa),
+        is mandatory;
     column gender =>
         type is 'text',
-        valid_values are qw(Mal Fem);
+        valid_values are qw(Mal Fem),
+        is mandatory;
     column alignment =>
         type is 'text',
-        valid_values are qw(Law Neu Cha);
+        valid_values are qw(Law Neu Cha),
+        is mandatory;
     column death =>
-        type is 'text';
+        type is 'text',
+        is mandatory;
     column ascended =>
-        is boolean;
+        is boolean,
+        default is 0,
+        is mandatory;
     column conduct =>
         type is 'int',
         default is 0;
