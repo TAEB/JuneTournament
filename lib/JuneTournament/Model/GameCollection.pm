42186347a18bcd008d0ba4312003cf7b9452588c
Shawn M Moore <sartak@gmail.com>
2008-04-17 17:39:16 -0400

    GameCollection->ascensions

--- lib/JuneTournament/Model/GameCollection.pm
+++ lib/JuneTournament/Model/GameCollection.pm
@@ -3,6 +3,16 @@ package JuneTournament::Model::GameCollection;
 use strict;
 use warnings;
 use parent 'JuneTournament::Collection';
+use Scalar::Util 'blessed';
+
+sub ascensions {
+    my $class = shift;
+    return $class->limit_to_ascensions if blessed $class;
+    my $ascs = $class->new;
+    $ascs->unlimit;
+    $ascs->limit_to_ascensions;
+    return $ascs;
+}
 
 sub limit_to_ascensions {
     my $self = shift;
