d5e7127336c19fdd92c5485425ce5d9b551855a1
Shawn M Moore <sartak@gmail.com>
2008-04-17 17:32:57 -0400

    GameCollection->binary_search

--- lib/JuneTournament/Model/GameCollection.pm
+++ lib/JuneTournament/Model/GameCollection.pm
@@ -10,5 +10,31 @@ sub limit_to_ascensions {
     return $self;
 }
 
+sub binary_search {
+    my $self = shift;
+    my $sub  = shift;
+
+    my $lo = 1;
+    my $hi = $self->count;
+
+    while ($lo < $hi) {
+        my $i = ($hi + $lo) >> 1;
+        $self->set_page_info(
+            per_page     => 1,
+            current_page => $i,
+        );
+        local ($_) = <$self>;
+
+        my $cmp = $sub->();
+
+        # if they're equal, then our rank should be higher, because earlier is
+        # better
+        if ($cmp >= 0) { $lo = $i + 1 }
+                  else { $hi = $i - 1 }
+    }
+
+    return $hi;
+}
+
 1;
 
