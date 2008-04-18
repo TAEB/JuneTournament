74d21469cfdce9eaf0d29f25114ebd22d8530aca
Shawn M Moore <sartak@gmail.com>
2008-04-17 22:21:20 -0400

    Sort all gamecollections by id, then endtime

--- lib/JuneTournament/Model/GameCollection.pm
+++ lib/JuneTournament/Model/GameCollection.pm
@@ -5,6 +5,13 @@ use warnings;
 use parent 'JuneTournament::Collection';
 use Scalar::Util 'blessed';
 
+sub implicit_clauses {
+    my $self = shift;
+
+    $self->order_by(column => 'id');
+    $self->add_order_by(column => 'endtime');
+}
+
 sub ascensions {
     my $class = shift;
     return $class->limit_to_ascensions if blessed $class;
